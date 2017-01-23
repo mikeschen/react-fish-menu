9e566b69c09d9f7399d64b3d9111524a86a843f0
Michael Schennum <mikeschenphoto@gmail.com>
2017-01-23 11:32:13 -0700

    adds main files

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.addFish = this.addFish.bind(this);
+		//get initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		const fishes = {...this.state.fishes};
+		const timestamp = Date.now();
+		console.log("fishes[before]", fishes[`fish-${timestamp}`]);
+		fishes[`fish-${timestamp}`] = fish;
+		console.log("fishes[after]", fishes[`fish-${timestamp}`]);
+		this.setState({ fishes });
+	}
+
+	render() {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header tagline="Fresh Seafood Market"/>
+				</div>
+					<Order />
+					<Inventory addFish={this.addFish} />
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault();
+		console.log("button clicked");
+		const storeId = this.storeInput.value;
+		console.log(`Going to ${storeId}`);
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
+	render() {
+		return (
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
+				<button type="submit">Visit Store</button>
+			</form>
+		)
+	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
+export default StorePicker;
