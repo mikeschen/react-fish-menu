5394ba9fcff66a0a5b5d79d010200cafce374c4b
Michael Schennum <mikeschenphoto@gmail.com>
2017-02-02 17:10:54 -0700

    adds your odrder

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		//get initial state
 		this.state = {
 			fishes: {},
@@ -32,6 +33,12 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key) {
+		const order = {...this.state.order};
+		order[key] = order[key] + 1 || 1;
+		this.setState({ order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -41,11 +48,11 @@ class App extends React.Component {
 						{
 							Object
 								.keys(this.state.fishes)
-								.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+								.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
 						}
 					</ul>
 				</div>
-					<Order />
+					<Order fishes={this.state.fishes} order={this.state.order} />
 					<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
 		)
