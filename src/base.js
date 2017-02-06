ad2547a4683f4a0c10723f0d3f2b8c81c18ac8b7
Michael Schennum <mikeschenphoto@gmail.com>
2017-02-06 14:08:15 -0700

    adds firebase connection

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCVXZrOq_lcaSo0KRXqKOsGnLiSREJEmdQ",
+    authDomain: "catch-of-the-day-schennum.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-schennum.firebaseio.com",
+});
+
+export default base;
